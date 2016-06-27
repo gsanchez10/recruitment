e3d5433f5a2f71de2abbce6d71bd7d209183f3c9
G Sanchez <gjsanchez@gbm.net>
2016-06-27 17:42:42 -0600

    agregue login con fb

--- MusicApp/myApp/www/js/controllers.js
+++ MusicApp/myApp/www/js/controllers.js
@@ -1,6 +1,98 @@
 angular.module('starter.controllers', [])
 
-.controller('LoginCtrl', function($scope, $location, LoginFunctions, $cookies) {
+.controller('LoginCtrl', function($scope, $rootScope, $location, LoginFunctions, $cookies, $cookieStore) {
+  function statusChangeCallback(response) {
+    console.log('statusChangeCallback');
+    console.log(response);
+    // The response object is returned with a status field that lets the
+    // app know the current login status of the person.
+    // Full docs on the response object can be found in the documentation
+    // for FB.getLoginStatus().
+    if (response.status === 'connected') {
+      $rootScope.globals = {
+          currentUser: {
+              username: response.data
+          }
+      };
+
+      //$http.defaults.headers.common['Authorization'] = 'Basic ' + authdata; // jshint ignore:line
+      $cookieStore.put('globals', $rootScope.globals);
+
+      //window.location = window.location.protocol + "//" + window.location.hostname + "/" + window.location.pathname + "#/tab/songs";
+      // Logged into your app and Facebook.
+      testAPI();
+    } else if (response.status === 'not_authorized') {
+      // The person is logged into Facebook, but not your app.
+      document.getElementById('status').innerHTML = 'Please log ' +
+        'into this app.';
+    } else {
+      // The person is not logged into Facebook, so we're not sure if
+      // they are logged into this app or not.
+      document.getElementById('status').innerHTML = 'Please log ' +
+        'into Facebook.';
+    }
+  }
+
+  // This function is called when someone finishes with the Login
+  // Button.  See the onlogin handler attached to it in the sample
+  // code below.
+  function checkLoginState() {
+    FB.getLoginStatus(function(response) {
+      statusChangeCallback(response);
+    });
+  }
+
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '117169485307087',
+      cookie     : true,  // enable cookies to allow the server to access 
+                          // the session
+      xfbml      : true,  // parse social plugins on this page
+      version    : 'v2.5' // use graph api version 2.5
+    });
+
+    // Now that we've initialized the JavaScript SDK, we call 
+    // FB.getLoginStatus().  This function gets the state of the
+    // person visiting this page and can return one of three states to
+    // the callback you provide.  They can be:
+    //
+    // 1. Logged into your app ('connected')
+    // 2. Logged into Facebook, but not your app ('not_authorized')
+    // 3. Not logged into Facebook and can't tell if they are logged into
+    //    your app or not.
+    //
+    // These three cases are handled in the callback function.
+
+    FB.getLoginStatus(function(response) {
+      statusChangeCallback(response);
+    });
+
+  };
+
+  // Load the SDK asynchronously
+  (function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = "//connect.facebook.net/en_US/sdk.js";
+    fjs.parentNode.insertBefore(js, fjs);
+  }(document, 'script', 'facebook-jssdk'));
+
+  // Here we run a very simple test of the Graph API after login is
+  // successful.  See statusChangeCallback() for when this call is made.
+  function testAPI() {
+    console.log('Welcome!  Fetching your information.... ');
+    FB.api('/me', function(response) {
+      console.log('Successfullllll login for: ' + response.name);
+      document.getElementById('status').innerHTML =
+        'Thanks for logging in, ' + response.name + '!';
+    });
+  }
+
+
+
+
+
   var cookie = $cookies.get('songUser');
   if(cookie != null) {
     $scope.user = JSON.parse(cookie);
@@ -23,7 +115,16 @@ angular.module('starter.controllers', [])
   
 })
 
-.controller('SongsCtrl', function($scope, SongsService) {
+.controller('SongsCtrl', function($scope, $rootScope, $location, SongsService, $cookieStore) {
+  $rootScope.globals = $cookieStore.get('globals') || {};
+  if(!$rootScope.globals) {
+    $location.path('/tab/login');
+  }else {
+    if(!$rootScope.globals.currentUser) {
+      $location.path('/tab/login');
+    }
+  }
+  
   SongsService.getAll(function(response){
     console.log(response);
     if(response.success == true) {
